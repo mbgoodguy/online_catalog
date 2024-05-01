00e032df9fb12dcc5c1a347c7372337af2ac09a8
mbgoodguy <nerooren4001@gmail.com>
2024-05-01 19:59:03 +0300

    Add field into EmployeeAdmin
    Now we can set supervisor using search field
    
    Without raw_id_fields we can't do it

--- employees/admin.py
+++ employees/admin.py
@@ -6,4 +6,8 @@ from employees.models import Employee
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
     fields = ('full_name', 'position', 'date_of_employment', 'supervisor', 'salary_in_dollars', )
-    readonly_fields = ['date_of_employment']
+    readonly_fields = ('date_of_employment', )
+    raw_id_fields = ('supervisor', )  #
+    search_fields = ('full_name', )  # параметр для поиска в окне выбора руководителя
+    list_filter = ('position', 'supervisor', )
+
