7b78ba25ac31dd58c79e011be9baa83a2a7690f6
mbgoodguy <nerooren4001@gmail.com>
2024-05-01 19:55:21 +0300

    Update template, add pagination

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,10 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+
+from employees.models import Employee
+
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 
 from employees.models import Employee
@@ -5,5 +12,18 @@ from employees.models import Employee
 
 def show_tree(request):
     employees = Employee.objects.all()
+    current_page = Paginator(list(employees), 20)
+    page = request.GET.get('page')  # Получаем параметр 'page' из GET-запроса
 
-    return render(request, 'tree.html', context={'employees': employees})
+    context = {}
+
+    try:
+        context['employees'] = current_page.page(page)
+    except PageNotAnInteger:
+        # Если параметр 'page' не является целым числом, показываем первую страницу
+        context['employees'] = current_page.page(1)
+    except EmptyPage:
+        # Если номер страницы выходит за пределы допустимых значений, показываем последнюю страницу
+        context['employees'] = current_page.page(current_page.num_pages)
+
+    return render(request, 'tree.html', context)
